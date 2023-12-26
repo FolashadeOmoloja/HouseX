2bc66ed21187e2cc3dc7c7e22753f1189c9cf472
FolashadeOmoloja <omolojashade@gmail.com>
2023-12-26 22:02:41 +0100

    navbar

---
+++ component/Home/Header/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <section>
+
+        </section>
+        <section>
+            
+        </section>
+    </header>
+  )
+}
+
+export default Header
