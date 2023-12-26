6ad19b6c4071d83177aa2c146d47c5b71b985a7d
FolashadeOmoloja <omolojashade@gmail.com>
2023-12-26 23:21:07 +0100

    header

--- components/Home/Header/Nav.tsx
+++ components/Home/Header/Nav.tsx
@@ -4,8 +4,8 @@ import React from 'react'
 const NavBar = () => {
   return (
     <nav>                     
-      <ul className="flex justify-between text-sm">
-          <li className='flex'> <h2>H<img src="home.png" alt="" className='w-4 h-4'/>useX</h2></li>
+      <ul className="flex justify-between ">
+          <li className='text-[24px] font-bold flex items-center '> <span>H</span><img src="home.png" alt="" className='w-5 h-5'/><span>useX</span></li>
           <li className="list-item">Properties</li>
           <li className="list-item">Buyers</li>
           <li className="list-item">Sellers</li>
