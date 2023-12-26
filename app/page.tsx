07d9009722e6eb8a55a8ba0ef6a406b4ef24598d
FolashadeOmoloja <omolojashade@gmail.com>
2023-12-26 22:27:22 +0100

    navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import Header from '@/component/Home/Header/Header'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="">
-             
+        <Header/>     
     </main>
   )
 }
--- component/Home/Header/Header.tsx
+++ component/Home/Header/Header.tsx
@@ -5,9 +5,7 @@ const Header = () => {
   return (
     <header>
         <MaxWidthWrapper >
-             <NavBar>
-                
-             </NavBar>
+             <NavBar/>
         </MaxWidthWrapper>
         <section>
 
