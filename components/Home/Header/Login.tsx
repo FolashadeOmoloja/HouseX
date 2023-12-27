2a915bac03b03aafb336f6177d96222e4cd04faf
FolashadeOmoloja <omolojashade@gmail.com>
2023-12-28 00:10:11 +0100

    header

--- components/Home/Header/Login.tsx
+++ components/Home/Header/Login.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Login = () => {
   return (
     <nav className='flex gap-8 nav pr-2.5 md:pr-20 justify-end box-shadow'>
-        <button>Login</button>
-        <button>Sign Up</button>
+        <button className='list-item'>Login</button>
+        <button className='px-7 py-[10px] rounded-[20px] bg-[#c0ccce] hover:bg-transparent hover:border-2 hover:border-[#c0ccce]'>Sign Up</button>
     </nav>
   )
 }
--- components/Home/Header/Nav.tsx
+++ components/Home/Header/Nav.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <nav className='nav  pl-2.5 md:pl-20 box-shadow'>                     
+    <nav className='nav  pl-2.5 md:pl-20 box-shadow '>                     
       <ul className="flex justify-between w-full ">
           <li className='text-[24px] font-bold flex items-center '> H<img src="home.png" alt="" className='w-5 h-5 mb-[2px]'/>useX</li>
           <li className="list-item"><Link href={'#'}>Properties</Link></li>
