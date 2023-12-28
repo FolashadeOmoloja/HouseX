bcf0993f89ef746961c96ba17f33382bfef56dbf
FolashadeOmoloja <omolojashade@gmail.com>
2023-12-28 22:36:57 +0100

    header

--- components/Home/Header/HeadLine.tsx
+++ components/Home/Header/HeadLine.tsx
@@ -11,7 +11,7 @@ const ptSerif = PT_Serif(
 const HeadLine = () => {
   return (
     <section>
-         <h1 className={`${ptSerif.className}}`}>Sell or rent your <span>Home</span> at the best prices</h1>
+         <h1 className={`${ptSerif.className} text-[44px] text-white font-bold`}>Sell or rent your <span>Home</span> at the best prices</h1>
          <h2>Discover a place you would love to live</h2>
          <p>Connect with more than 75 million renters looking for new homes<br/>using our comprehensive marketing platform</p>
     </section>
--- components/Home/Header/Header.tsx
+++ components/Home/Header/Header.tsx
@@ -1,12 +1,14 @@
 import MaxWidthWrapper from '@/components/Elements/MaxWidthWrapper'
 import NavBar from './Nav'
 import Login from './Login'
+import HeadLine from './HeadLine'
 
 const Header = () => {
   return (
     <header className='flex md:h-[100vh]'>
         <section className='first-headersection basis-3/5'>
              <NavBar/>
+             <HeadLine/>
         </section>
         <section className='basis-2/5 bg-cover bg-no-repeat bg-[#5E686A] background-position' style={{backgroundImage: `url('wallpaper.jpg')`}}>
              <Login/>
