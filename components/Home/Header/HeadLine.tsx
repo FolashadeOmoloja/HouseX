5c9271b4ac2bcf90339d1fca08dafd4ddf01f09e
FolashadeOmoloja <omolojashade@gmail.com>
2023-12-29 16:07:43 +0100

    header

--- components/Home/Header/HeadLine.tsx
+++ components/Home/Header/HeadLine.tsx
@@ -1,4 +1,5 @@
 import { PT_Serif } from 'next/font/google'
+import { FaSearchLocation } from "react-icons/fa";
 
 const ptSerif = PT_Serif(
   {
@@ -10,10 +11,16 @@ const ptSerif = PT_Serif(
 
 const HeadLine = () => {
   return (
-    <section>
-         <h1 className={`${ptSerif.className} text-[44px] text-white font-bold`}>Sell or rent your <span>Home</span> at the best prices</h1>
-         <h2>Discover a place you would love to live</h2>
+    <section className='maxWidthWrapper py-20'>
+         <h1 className={`${ptSerif.className} text-[44px] text-white font-bold`}>Sell or rent your
+         <br /> <span className='text-black'>Home</span> at the best prices</h1>
+         <h2 className='mt-10'>Discover a place you would love to live</h2>
          <p>Connect with more than 75 million renters looking for new homes<br/>using our comprehensive marketing platform</p>
+         <section className=''>
+             <div className='w-14 h-14 bg-white text-[#c0ccce] flex div-center rounded-2xl'>
+              <div className='w-11 h-11 bg-[#5d6769] rounded-2xl flex div-center'><FaSearchLocation/></div>
+             </div>
+         </section>
     </section>
   )
 }
