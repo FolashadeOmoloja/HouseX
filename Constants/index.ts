d2b9c4b639a66b23d0b4a223453c15981e2d7455
FolashadeOmoloja <omolojashade@gmail.com>
2024-01-02 15:04:04 +0100

    header

---
+++ Constants/index.ts
@@ -0,0 +1,7 @@
+const properties = [
+    {value:'residential', options:'Residential House'},
+    {value:'apartment', options:'Apartments'},
+    {value:'commercial', options:'Commercial Building'},
+    {value:'condo', options:'Condominium'},
+    {value:'townHouse', options:'TownHouse'},
+]
--- components/Elements/SearchBar.tsx
+++ components/Elements/SearchBar.tsx
@@ -1,12 +1,28 @@
-import React from 'react'
+'use client'
+import { useState } from 'react'
 import { FaSearchLocation } from 'react-icons/fa'
 
 const SearchBar = () => {
   return (
-    <div className='w-14 h-14 bg-white text-[#c3d4cc] flex div-center rounded-2xl'>
-    <div className='w-11 h-11 bg-[#7e9999] rounded-2xl flex div-center text-[20px]'>
+    <div className='w-14 h-14 bg-white text-[#c3d4cc] flex div-center rounded-2xl transtion ease duration-500'>
+    <section className='w-11 h-11 bg-[#7e9999] rounded-2xl flex div-center text-[20px]'>
+        <section>
+
+        </section>
+        <section>
+        <div>
+        <label htmlFor="property">Property Type</label>
+             <select name="properties" id="property">
+                 <option value="residential">Residential House</option>
+                 <option value="apartment">Apartments</option>
+                 <option value="commercial">Commercial Building</option>
+                 <option value="condo">Condominium</option>
+                 <option value="land">TownHouse</option>
+             </select>
+        </div>
+        </section>
         <FaSearchLocation/>
-    </div>
+    </section>
    </div>
   )
 }
