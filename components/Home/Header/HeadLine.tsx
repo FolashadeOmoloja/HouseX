ef01eac926ebcc2919a6af22b45d38811d5bb462
FolashadeOmoloja <omolojashade@gmail.com>
2024-01-04 21:13:33 +0100

    header

--- components/Home/Header/HeadLine.tsx
+++ components/Home/Header/HeadLine.tsx
@@ -13,8 +13,8 @@ const ptSerif = PT_Serif(
 
 const HeadLine = () => {
   return (
-    <section className='maxWidthWrapper py-20 max-xxlg:p-[52px] max-xslg:div-center flex-col max-xslg:text-center max-w-[100vw]'>
-         <h1 className={`${ptSerif.className} text-[44px] text-white font-bold`}>Sell or rent your
+    <section className='maxWidthWrapper py-20 max-xxlg:p-[52px] max-md:p-8 max-xsm:p-4 max-xslg:div-center flex-col max-xslg:text-center max-w-[100vw]'>
+         <h1 className={`${ptSerif.className} text-[44px] text-white font-bold max-xsm:text-[32px]`}>Sell or rent your
          <br /> <span className='text-black'>Home</span> at the best prices</h1>
          <p className='mt-5'>
          HouseX streamlines rental management in Africa, offering free listings, flexible payments, and automated tools for renters and landlords. Discover your ideal home and connect with millions through our robust marketing platform.</p>
@@ -23,7 +23,7 @@ const HeadLine = () => {
          </section>
          <section className='flex mt-10 gap-2'>
           <span>Popular Search: </span>
-          <div className='flex gap-3'>
+          <div className='flex gap-3 max-md:flex-wrap'>
             {
               popularSearches.map((item, idx)=>{
                 return <span className='text-sm px-2 py-1 rounded-lg bg-[#ffffff73]' key={idx}>{item}</span>
--- components/Home/Header/Nav.tsx
+++ components/Home/Header/Nav.tsx
@@ -6,13 +6,13 @@ import Login from './Login'
 const NavBar = () => {
   return (
     <nav className='nav  pl-2.5 md:pl-20 box-shadow '>                     
-      <ul className="flex justify-between w-full ">
+      <ul className="flex justify-between w-full  ">
           <li className='text-[24px] font-bold flex items-center '> H<img src="home.png" alt="" className='w-5 h-5 mb-[2px]'/>useX</li>
-          <li className="list-item"><Link href={'#'}>Properties</Link></li>
-          <li className="list-item"><Link href={'#'}>Buyers</Link></li>
-          <li className="list-item"><Link href={'#'}>Sellers</Link></li>
-          <li className="list-item"><Link href={'#'}>Valuation</Link></li>
-          <li className="list-item"><Link href={'#'}>Contact</Link></li>
+          <li className="list-item max-xslg:hidden"><Link href={'#'}>Properties</Link></li>
+          <li className="list-item max-xslg:hidden"><Link href={'#'}>Buyers</Link></li>
+          <li className="list-item max-xslg:hidden"><Link href={'#'}>Sellers</Link></li>
+          <li className="list-item max-xslg:hidden"><Link href={'#'}>Valuation</Link></li>
+          <li className="list-item max-xslg:hidden"><Link href={'#'}>Contact</Link></li>
       </ul>
     </nav>
   )
