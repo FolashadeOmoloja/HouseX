1e6f9d5beb11c159622328f8c869bc6410a1f798
FolashadeOmoloja <omolojashade@gmail.com>
2024-01-04 22:57:59 +0100

    header

--- components/Home/Header/Header.tsx
+++ components/Home/Header/Header.tsx
@@ -5,14 +5,16 @@ import HeadLine from './HeadLine'
 
 const Header = () => {
   return (
-    <header className='flex xlg:h-[100vh] headersection-gradient max-xslg:flex-col'>
-        <section className=' basis-3/5'>
-             <NavBar/>
-             <HeadLine/>
-        </section>
-        <section className='basis-2/5 bg-cover bg-no-repeat  background-position max-xslg:hidden ' style={{backgroundImage: `url('wallpaper.png')`}}>
-             <Login/>
-        </section>
+    <header className='xlg:h-[100vh] headersection-gradient max-w-[1440px] mx-auto'>
+       <section className='flex  w-full max-xslg:flex-col'>
+           <section className=' basis-3/5'>
+                 <NavBar/>
+                 <HeadLine/>
+            </section>
+            <section className='basis-2/5 bg-cover bg-no-repeat  background-position max-xslg:hidden ' style={{backgroundImage: `url('wallpaper.png')`}}>
+                 <Login/>
+            </section>
+           </section>
     </header>
   )
 }
