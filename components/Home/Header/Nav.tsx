3284ac9491c50c726bb7ba3b4f459f37308112a7
FolashadeOmoloja <omolojashade@gmail.com>
2024-01-04 21:32:43 +0100

    header

--- components/Home/Header/Nav.tsx
+++ components/Home/Header/Nav.tsx
@@ -5,7 +5,7 @@ import Login from './Login'
 
 const NavBar = () => {
   return (
-    <nav className='nav  pl-2.5 md:pl-20 box-shadow '>                     
+    <nav className='nav  pl-4 md:pl-20 box-shadow '>                     
       <ul className="flex justify-between w-full  ">
           <li className='text-[24px] font-bold flex items-center '> H<img src="home.png" alt="" className='w-5 h-5 mb-[2px]'/>useX</li>
           <li className="list-item max-xslg:hidden"><Link href={'#'}>Properties</Link></li>
