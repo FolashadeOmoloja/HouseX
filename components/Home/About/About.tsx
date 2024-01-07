eb642bf6d0931c74c487440378eba3716932956d
FolashadeOmoloja <omolojashade@gmail.com>
2024-01-07 22:51:51 +0100

    header

--- components/Home/About/About.tsx
+++ components/Home/About/About.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 const About = () => {
   return (
     <main className='maxWidthWrapper bg-white'>
-        <section className='flex justify-between items-end'>
-            <h2 className='basis-3/5 text-[40px]'>Your Trusted Real Estate <br /> Marketplace</h2>
-            <p className='text-sm text-[#4b5456] basis-2/5'>Your cutting-edge real estate marketplace that offers a seamless and immersive experience for finding your dream home. A safespace for Londlords to enlist their properties abd verify potential buyers and sellers. </p>
+        <section className='flex justify-between items-end max-slg:flex-col'>
+            <h2 className='basis-3/5 text-[40px] max-slg:text-3xl'>Your Trusted Real Estate <br className='max-xslg:hidden' /> Marketplace</h2>
+            <p className='text-sm text-[#4b5456] basis-2/5'>Your cutting-edge real estate marketplace that offers a seamless and immersive experience for finding your dream home. A safespace for Londlords to enlist their properties and verify potential buyers or renters. </p>
         </section>
         <section>
 
