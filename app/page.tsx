4ded624ba15d0fbceb855b87be9f5344f55ac3e6
FolashadeOmoloja <omolojashade@gmail.com>
2024-01-11 23:26:05 +0100

    about

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import About from '@/components/Home/About/About'
 import Features from '@/components/Home/Features/Features'
 import Footer from '@/components/Home/Footer/Footer'
 import Header from '@/components/Home/Header/Header'
-import Image from 'next/image'
+
 
 export default function Home() {
   return (
