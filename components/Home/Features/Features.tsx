0f82dddfc9c4f9971974b67cab945ac3347876c3
FolashadeOmoloja <omolojashade@gmail.com>
2024-01-12 13:41:36 +0100

    about

--- components/Home/Features/Features.tsx
+++ components/Home/Features/Features.tsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Features = () => {
   return (
-   <main>
-    Features
+   <main className='maxWidthWrapper bg-[#f3f6fd]'>
+        <h3 className='text-[40px] max-slg:text-3xl  max-xsm:text-2xl'>Looking to rent or lease an apartment? <br className='max-xslg:hidden' /> We’ve got you covered.</h3>
+        <section className='mt-12'>
+                  <div className=' uppercase flex flex-col gap-7'>
+                    
+                     <div className='text-lg'>For Renters</div>
+                     <div>For Sellers</div>
+                  </div>
+        </section>
    </main>
   )
 }
