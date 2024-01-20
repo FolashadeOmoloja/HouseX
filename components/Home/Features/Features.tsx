61451be2b55e99c6e74c0e76fce81756c63f619b
FolashadeOmoloja <omolojashade@gmail.com>
2024-01-20 23:42:05 +0100

    about

--- components/Home/Features/Features.tsx
+++ components/Home/Features/Features.tsx
@@ -11,11 +11,11 @@ const Features = () => {
                      <div className='pl-3 border-l-[#7e9999] border-l-4 hover:text-[#7e9999] cursor-pointer transition'>For Sellers</div>
                   </div>
                   <section>
-                         <div className='bg-red-300 h-[450px] w-[400px] rounded-[20px] relative'>
-                            <img src="" alt="" />
+                         <div className='headersection-gradient h-[450px] w-[400px] rounded-[20px] relative'>
+                            <img src="house-img1.png" alt="" className="w-full h-full rounded-[20px]" />
                              <div className="right-angle-triangle absolute left-0 top-0"> </div>   
                              <div className="right-angle-bottom-triangle absolute bottom-0 right-0"> </div> 
-                             <div className='w-0 h-0 border-l-[130px] border-l-transparent border-b-[130px] border-b-[#7e9999]  rounded-br-[20px] absolute bottom-0 right-0'>
+                             <div  className='w-0 h-0 border-l-[130px] border-l-transparent border-b-[130px] border-b-[#7e9999]  rounded-br-[20px] absolute bottom-0 right-0'>
                              </div> 
                              <div className='w-0 h-0 border-l-[115px] border-l-transparent border-b-[115px] border-b-[#f3f6fd] rounded-br-[20px] absolute bottom-[4px] right-[4.7px] '>
                                <FaArrowRight className='text-[#7e9999] absolute right-[25px] top-[59px]'/>
