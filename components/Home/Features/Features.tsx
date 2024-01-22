d1cfd54f5065844e0f58abe89e7ddc09dac5f0dc
FolashadeOmoloja <omolojashade@gmail.com>
2024-01-22 22:18:06 +0100

    about

--- components/Home/Features/Features.tsx
+++ components/Home/Features/Features.tsx
@@ -1,5 +1,6 @@
 import { ImgShapeBox } from "@/components/Elements/ShapedBox";
 import { CgMenuGridR } from "react-icons/cg";
+import { FaUserFriends } from "react-icons/fa";
 
 const Features = () => {
   return (
@@ -15,15 +16,22 @@ const Features = () => {
                       <div className="w-[400px]">
                           <ImgShapeBox/>
                           <div className="mt-8">
-                             <div className="flex items-center gap-3 mb-2">
+                             <div className="flex items-center gap-3 mb-2 cursor-pointer">
                                  <CgMenuGridR className='text-[#7e9999]'/>
                                  <span>MarketPlace</span>
                              </div>
-                             <p className="text-sm">Effortless apartment hunting in Nigeria! Explore our curated listings, pick your perfect space, and enjoy flexible payment options—monthly, quarterly, or annually. No inspections, agency fees, or legal hassles. Simply pay and move in!</p>
+                             <p className="text-sm ">Effortless apartment hunting! Explore our curated listings, pick your perfect space, and enjoy flexible payment options. No agency fees or legal hassles. Simply pay and move in!</p>
                           </div>
                       </div>
                         <div className="w-[400px]">
                             <ImgShapeBox img="tenets.png"/>
+                            <div className="mt-8">
+                             <div className="flex items-center gap-3 mb-2 cursor-pointer">
+                                 <FaUserFriends className='text-[#7e9999]'/>
+                                 <span>Roomies Pairing</span>
+                             </div>
+                             <p className="text-sm ">Simplify apartment hunting in Nigeria! Our system pairs you with potential roommates based on shared interests and ideal fits, easing the burden of rents.</p>
+                          </div>
                         </div>
                       
                   </section>
