a70f458320d45cd5627c7902816c80076a734680
FolashadeOmoloja <omolojashade@gmail.com>
2024-01-23 15:59:12 +0100

    about

--- components/Home/Features/Features.tsx
+++ components/Home/Features/Features.tsx
@@ -6,11 +6,11 @@ const Features = () => {
   return (
    <main className='maxWidthWrapper bg-[#f3f6fd] text-[#4b5456]'>
         <h3 className='text-[40px] max-slg:text-3xl  max-xsm:text-2xl'>Looking to rent or lease an apartment? <br className='max-xslg:hidden' /> We’ve got you covered.</h3>
-        <section className='mt-12 flex relative'>
-                  <div className='uppercase flex flex-col gap-7 text-[17px] basis-[20%] ' >
+        <section className='mt-12 flex relative max-xlg:flex-col'>
+                  <div className='uppercase flex xlg:flex-col gap-7 text-[17px] basis-[20%] mx-auto max-xlg:mb-10' >
                     
-                     <div className='pl-3 border-l-[#7e9999] border-l-4 hover:text-[#7e9999] cursor-pointer transition'>For Renters</div>
-                     <div className='pl-3 border-l-[#7e9999] border-l-4 hover:text-[#7e9999] cursor-pointer transition'>For Sellers</div>
+                     <div className='features-switch'>For Renters</div>
+                     <div className='features-switch'>For Sellers</div>
                   </div>
                       <section className="flex justify-between basis-[80%] ">
                           <div className="w-[400px]">
