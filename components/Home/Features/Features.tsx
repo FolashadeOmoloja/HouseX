2441551556e0afdc8a505da47c00344c1c17ff3d
FolashadeOmoloja <omolojashade@gmail.com>
2024-01-23 16:11:57 +0100

    about

--- components/Home/Features/Features.tsx
+++ components/Home/Features/Features.tsx
@@ -4,19 +4,18 @@ import { FaUserFriends } from "react-icons/fa";
 
 const Features = () => {
   return (
-   <main className='maxWidthWrapper bg-[#f3f6fd] text-[#4b5456]'>
+   <main className='maxWidthWrapper bg-[#f3f6fd] text-[#4b5456] max-xlg:text-center'>
         <h3 className='text-[40px] max-slg:text-3xl  max-xsm:text-2xl'>Looking to rent or lease an apartment? <br className='max-xslg:hidden' /> We’ve got you covered.</h3>
         <section className='mt-12 flex relative max-xlg:flex-col'>
                   <div className='uppercase flex xlg:flex-col gap-7 text-[17px] basis-[20%] mx-auto max-xlg:mb-10' >
-                    
                      <div className='features-switch'>For Renters</div>
                      <div className='features-switch'>For Sellers</div>
                   </div>
-                      <section className="flex justify-between basis-[80%] ">
+                      <section className="flex xlg:justify-between basis-[80%] flex-wrap justify-center max-xslg:gap-8">
                           <div className="w-[400px]">
                               <ImgShapeBox/>
                               <div className="mt-8">
-                                 <div className="flex items-center gap-3 mb-2 cursor-pointer">
+                                 <div className="features-header">
                                      <CgMenuGridR className='text-[#7e9999]'/>
                                      <span>MarketPlace</span>
                                  </div>
@@ -26,7 +25,7 @@ const Features = () => {
                             <div className="w-[400px]">
                                 <ImgShapeBox img="tenets.png"/>
                                 <div className="mt-8">
-                                 <div className="flex items-center gap-3 mb-2 cursor-pointer">
+                                 <div className="features-header">
                                      <FaUserFriends className='text-[#7e9999]'/>
                                      <span>Roomies Pairing</span>
                                  </div>
