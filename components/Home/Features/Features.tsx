5db07a868fb9ac8f20bbf08d4833622bbc741c93
FolashadeOmoloja <omolojashade@gmail.com>
2024-01-24 12:51:36 +0100

    about

--- components/Home/Features/Features.tsx
+++ components/Home/Features/Features.tsx
@@ -1,19 +1,33 @@
-
+"use client"
+import { useState } from "react";
 import ForSellers from "./ForSellers";
+import ForRenters from "./ForRenters";
+import Login from "../Header/Login";
 
 const Features = () => {
+    const [forRenters, setForRenters] = useState(true)
   return (
    <main className='maxWidthWrapper bg-[#f3f6fd] text-[#4b5456] max-xlg:text-center'>
         <h3 className='text-[40px] max-slg:text-3xl  max-xsm:text-2xl'>Looking to rent or lease an apartment? <br className='max-xslg:hidden' /> We’ve got you covered.</h3>
-        <section className='mt-12 flex relative max-xlg:flex-col'>
+        <section className='mt-12 flex relative max-xlg:flex-col xlg:gap-10'>
                   <div className='uppercase flex xlg:flex-col xlg:gap-7 gap-4  text-[17px] basis-[20%] mx-auto max-xlg:mb-10 max-xlg:border-[#7e9999] max-xlg:border max-xlg:rounded-3xl' >
                      <div className='features-switch features-switch-active'>For Renters</div>
                      <div className='features-switch '>For Sellers</div>
                   </div>
                       <section className=" basis-[80%] ">
-                          <ForSellers/>
+                          {
+                          forRenters? (
+                           <ForRenters/>
+                          ): (
+                             <ForSellers/>
+                          )
+                          
+                        }
                       </section>
-                  </section>
+        </section>
+        <div className=" mt-16">
+                    <Login display="hidden" cta="Get Started" justifyEnd={false} width="w-[300px]"/>
+        </div>
    </main>
   )
 }
