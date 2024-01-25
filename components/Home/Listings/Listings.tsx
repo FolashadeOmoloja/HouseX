eb1687deedd5e529b2c20b994156cfea965ac656
FolashadeOmoloja <omolojashade@gmail.com>
2024-01-25 12:45:36 +0100

    features

--- components/Home/Listings/Listings.tsx
+++ components/Home/Listings/Listings.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const Listings = () => {
   return (
-    <section className='maxWidthWrapper  text-[#4b5456] max-xlg:text-center'>
-         <h2 className='text-[40px] max-slg:text-3xl  max-xsm:text-2xl'>Our Best Lisitings</h2>
-    </section>
+    <main className='maxWidthWrapper  text-[#4b5456] max-xlg:text-center'>
+         <h2 className='text-[40px] max-slg:text-3xl  max-xsm:text-2xl text-transparent bg-clip-text headersection-gradient'>Our Top Deals & Best Lisitings</h2>
+         <section>
+             
+         </section>
+    </main>
   )
 }
 
