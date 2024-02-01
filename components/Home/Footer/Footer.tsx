45a59431ed23f1b78ddb75be4ca040a3f689f56d
FolashadeOmoloja <omolojashade@gmail.com>
2024-02-01 21:05:33 +0100

    footing

--- components/Home/Footer/Footer.tsx
+++ components/Home/Footer/Footer.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-     <footer>
-            <main>
+     <footer className='bg-[#f3f6fd] px-4'>
+            <main className='bg-white rounded-3xl'>
                 <section>
                       <span>HouseX</span>
                       <p>YOUR BEST <br /> REAL ESTATE <br /> MARKETPLACE</p>
