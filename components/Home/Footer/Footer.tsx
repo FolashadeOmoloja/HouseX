716fb3f4d0a420c393c828e9e19b8da3817a0494
FolashadeOmoloja <omolojashade@gmail.com>
2024-02-01 21:11:29 +0100

    footing

--- components/Home/Footer/Footer.tsx
+++ components/Home/Footer/Footer.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-     <footer className='bg-[#f3f6fd] px-4'>
-            <main className='bg-white rounded-3xl'>
+     <footer className='bg-[#f3f6fd] px-4 mt-24'>
+            <main className='bg-white rounded-3xl maxWidthWrapper'>
                 <section>
                       <span className='text-[24px] font-bold flex items-center '>H<img src="home.png" alt="" className='w-5 h-5 mb-[2px]'/>useX</span>
                       <p>YOUR BEST <br /> REAL ESTATE <br /> MARKETPLACE</p>
