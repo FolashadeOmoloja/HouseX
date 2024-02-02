6acc3c26eb25c9fa8c8763e8f7cb06f71995fd1e
FolashadeOmoloja <omolojashade@gmail.com>
2024-02-02 13:37:11 +0100

    footing

--- components/Home/Footer/Footer.tsx
+++ components/Home/Footer/Footer.tsx
@@ -3,16 +3,31 @@ import { MdCopyright } from "react-icons/md";
 const Footer = () => {
   return (
      <footer className='bg-[#f3f6fd] px-4 mt-24 text-[#4b5456] pt-20'>
-            <main className='bg-white rounded-3xl px-2.5 md:px-20 md:py-10 py-2.5 mx-auto h-[400px]'>
+            <main className='bg-white rounded-3xl px-2.5 md:px-20 md:py-10 py-2.5 mx-auto h-[400px] flex justify-between'>
                 <section className='flex flex-col h-full justify-between'>
                       <div>
                           <span className='text-[24px] font-bold flex items-center '>H<img src="home.png" alt="" className='w-5 h-5 mb-[2px]'/>useX</span>
                           <p className='text-[32px] mt-8 text-transparent bg-clip-text headersection-gradient'>YOUR BEST <br /> REAL ESTATE <br /> MARKETPLACE</p>
                           </div>
-                      <span className=''>copywrite<MdCopyright />2024</span>
+                      <span className='flex items-center'><MdCopyright />2024</span>
                 </section>
                 <section>
-                    
+                      <div className="flex">
+                         <ul>
+                            <li>Services</li>
+                            <li>Team</li>
+                            <li>Company</li>
+                         </ul>
+                         <ul>
+                            <li>Contact</li>
+                            <li>Location</li>
+                         </ul>
+                         <ul>
+                            <li>About</li>
+                            <li>Policy</li>
+                            <li>Headquarters</li>
+                         </ul>
+                      </div>
                 </section>
             </main>
 
