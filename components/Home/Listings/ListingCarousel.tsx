323998082c34b5767d75ba6fb4725b2580ccb06b
FolashadeOmoloja <omolojashade@gmail.com>
2024-02-06 00:02:27 +0100

    listings

--- components/Home/Listings/ListingCarousel.tsx
+++ components/Home/Listings/ListingCarousel.tsx
@@ -16,7 +16,7 @@ const ListingCarousel:React.FC<CarouselProps> = ({items}) => {
     const [isDragging, setIsDragging] = useState(false);
     const [prevPageX, setPrevPageX] = useState(0);
     const [prevScrollLeft, setPrevScrollLeft] = useState(0);
-    const [positionDiff, setPositionDiff] = useState(0);
+    // const [positionDiff, setPositionDiff] = useState(0);
 
 
     useEffect(() => {
@@ -25,15 +25,40 @@ const ListingCarousel:React.FC<CarouselProps> = ({items}) => {
         const showHideIcons = () => {
             const scrollWidth = carousel.scrollWidth - carousel.clientWidth;
             // Assuming you have left and right icons as children of the carousel element
-            const leftIcon = carousel.previousElementSibling as HTMLElement;
-            const rightIcon = carousel.nextElementSibling as HTMLElement;
-      
+            const leftIcon = carousel.firstElementChild as HTMLElement;
+            const rightIcon = carousel.lastElementChild as HTMLElement;
+            leftIcon.style.display = carousel.scrollLeft === 0 ? 'none' : 'block';
+            rightIcon.style.display = carousel.scrollLeft === scrollWidth ? 'none' : 'block';
+            
         };
+
+        const autoSlide = () => {
+            if (
+              carousel.scrollLeft - (carousel.scrollWidth - carousel.clientWidth) > -1 ||
+              carousel.scrollLeft <= 0
+            )
+              return;
+            let positionDiff = 0
+            positionDiff = Math.abs(positionDiff);
+            const firstImgWidth = carousel.firstElementChild?.clientWidth + 14 || 0;
+            const valDifference = firstImgWidth - positionDiff;
+      
+            if (carousel.scrollLeft > prevScrollLeft) {
+              return (
+                (carousel.scrollLeft +=
+                  positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff),
+                showHideIcons()
+              );
+            }
+      
+            carousel.scrollLeft -= positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+            showHideIcons();
+          };
       }, []);
   
   return (
     <section className='mt-20 relative' ref={carouselRef}>
-    <button ref={leftButton}><FaChevronCircleLeft className=' left-[-22px]  slider-icon  ' /></button>
+    <button><FaChevronCircleLeft className=' left-[-22px]  slider-icon  ' /></button>
       <section className='div-center gap-5 overflow-x-scroll cursor-grab'>
                  {
                   items.map((item, idx)=>{
@@ -42,7 +67,7 @@ const ListingCarousel:React.FC<CarouselProps> = ({items}) => {
                  }
 
       </section>
-      <button ref={rightButton}><FaChevronCircleRight className=' right-[-22px] slider-icon '/></button>
+      <button><FaChevronCircleRight className=' right-[-22px] slider-icon '/></button>
  </section>
   )
 }
