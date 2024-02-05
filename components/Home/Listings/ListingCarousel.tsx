cac40f92b39949e6fcd89360a9c83520343672a2
FolashadeOmoloja <omolojashade@gmail.com>
2024-02-05 21:32:08 +0100

    listings

--- components/Home/Listings/ListingCarousel.tsx
+++ components/Home/Listings/ListingCarousel.tsx
@@ -1,14 +1,31 @@
+"use client"
 import { ListingBox } from '@/components/Elements/ShapedBox'
-import React from 'react'
 import { FaChevronCircleLeft, FaChevronCircleRight } from 'react-icons/fa'
+import { useEffect, useRef, useState } from 'react';
 
 interface CarouselProps {
     items: string[]; 
   }
 
 const ListingCarousel:React.FC<CarouselProps> = ({items}) => {
+
+    const carouselRef = useRef<HTMLDivElement>(null);
+    const [isDragStart, setIsDragStart] = useState(false);
+    const [isDragging, setIsDragging] = useState(false);
+    const [prevPageX, setPrevPageX] = useState(0);
+    const [prevScrollLeft, setPrevScrollLeft] = useState(0);
+    const [positionDiff, setPositionDiff] = useState(0);
+
+
+    useEffect(() => {
+        const carousel = carouselRef.current;
+    
+        const showHideIcons = () => {
+        };
+      }, []);
+  
   return (
-    <section className='mt-20 relative'>
+    <section className='mt-20 relative' ref={carouselRef}>
     <button><FaChevronCircleLeft className=' left-[-22px]  slider-icon  ' /></button>
       <section className='div-center gap-5 overflow-x-scroll cursor-grab'>
                  {
