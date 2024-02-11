dea99e3c149f796a517958ac9209d0c7334cf33d
FolashadeOmoloja <omolojashade@gmail.com>
2024-02-11 23:37:19 +0100

    listings

--- components/Dashboard/Home.tsx
+++ components/Dashboard/Home.tsx
@@ -1,8 +1,14 @@
+
+
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <main>
+        <div></div>
+        <div></div>
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci inventore excepturi dolor at illo. Laudantium, quo veniam obcaecati veritatis quia, odit doloribus explicabo, laborum omnis maiores earum velit libero aperiam.</div>
+    </main>
   )
 }
 
