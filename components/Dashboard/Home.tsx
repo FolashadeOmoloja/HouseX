0c956fa56f65e73f0fe33a5d0f4512ea96e6919d
FolashadeOmoloja <omolojashade@gmail.com>
2024-02-11 23:38:04 +0100

    listings

--- components/Dashboard/Home.tsx
+++ components/Dashboard/Home.tsx
@@ -8,6 +8,8 @@ const Home = () => {
         <div></div>
         <div></div>
         <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci inventore excepturi dolor at illo. Laudantium, quo veniam obcaecati veritatis quia, odit doloribus explicabo, laborum omnis maiores earum velit libero aperiam.</div>
+        <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati itaque placeat saepe culpa amet. Temporibus officia veniam vel dolor modi sapiente blanditiis velit perspiciatis esse, non repellendus dignissimos recusandae molestiae!
+        Omnis voluptatem sed nisi molestias unde quo aut quis. Delectus laboriosam fuga facilis voluptas nostrum repellat quisquam fugit minus debitis, sunt amet dolor quibusdam possimus. Cum iste laboriosam cumque nam!</div>
     </main>
   )
 }
