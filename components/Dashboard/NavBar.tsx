b613dedb1408c1a314d26f1790da0afe41d8bdad
FolashadeOmoloja <omolojashade@gmail.com>
2024-02-14 22:26:40 +0100

    listings

--- components/Dashboard/NavBar.tsx
+++ components/Dashboard/NavBar.tsx
@@ -4,6 +4,15 @@ const NavBar = () => {
   return (
     <nav>
         <ul>
+            <li>Home</li>
+            <li>Church</li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
             <li></li>
         </ul>
     </nav>
