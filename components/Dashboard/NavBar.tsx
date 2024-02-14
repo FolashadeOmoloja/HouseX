c67fa97664d206d724b82c09c16141cfad2fe6b5
FolashadeOmoloja <omolojashade@gmail.com>
2024-02-14 22:25:27 +0100

    listings

--- components/Dashboard/NavBar.tsx
+++ components/Dashboard/NavBar.tsx
@@ -3,57 +3,9 @@ import React from 'react'
 const NavBar = () => {
   return (
     <nav>
-   <section>
-          <h3 className="text-[30px] font-semibold text-[#2387FE]">Customer Support</h3>
-          <span className="text-sm mb-5 font-semibold">We're here to help!</span>
-          <section className="flex max-md:flex-col">
-              <div className="basis-1/2">
-                 <img src="/customercare.webp" alt="img" />
-              </div>
-              <div className="basis-1/2">
-              </div>
-          </section>
-          <section className="mt-8 flex flex-col">
-            <span className="text-[30px] font-semibold text-[#2387FE]">Have any Complaints? Let us know here</span>
-            <span className="text-sm  mb-5 font-semibold">You can also talk to our representatives via whatsapp or email</span>
-          </section>
-          <div className="fixed right-[20px] bottom-[50px] ">
-        </div>
-   </section>
-   <section>
-          <h3 className="text-[30px] font-semibold text-[#2387FE]">Customer Support</h3>
-          <span className="text-sm mb-5 font-semibold">We're here to help!</span>
-          <section className="flex max-md:flex-col">
-              <div className="basis-1/2">
-                 <img src="/customercare.webp" alt="img" />
-              </div>
-              <div className="basis-1/2">
-              </div>
-          </section>
-          <section className="mt-8 flex flex-col">
-            <span className="text-[30px] font-semibold text-[#2387FE]">Have any Complaints? Let us know here</span>
-            <span className="text-sm  mb-5 font-semibold">You can also talk to our representatives via whatsapp or email</span>
-          </section>
-          <div className="fixed right-[20px] bottom-[50px] ">
-        </div>
-   </section>
-   <section>
-          <h3 className="text-[30px] font-semibold text-[#2387FE]">Customer Support</h3>
-          <span className="text-sm mb-5 font-semibold">We're here to help!</span>
-          <section className="flex max-md:flex-col">
-              <div className="basis-1/2">
-                 <img src="/customercare.webp" alt="img" />
-              </div>
-              <div className="basis-1/2">
-              </div>
-          </section>
-          <section className="mt-8 flex flex-col">
-            <span className="text-[30px] font-semibold text-[#2387FE]">Have any Complaints? Let us know here</span>
-            <span className="text-sm  mb-5 font-semibold">You can also talk to our representatives via whatsapp or email</span>
-          </section>
-          <div className="fixed right-[20px] bottom-[50px] ">
-        </div>
-   </section>
+        <ul>
+            <li></li>
+        </ul>
     </nav>
   )
 }
